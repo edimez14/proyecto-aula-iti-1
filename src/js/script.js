e95282ca5cd65f52f5789d636b30ad50fa4dc954
edimez14 <edimez14@gmail.com>
2024-05-26 11:15:31 -0500

    menu ya funciona

--- src/js/script.js
+++ src/js/script.js
@@ -17,10 +17,10 @@ function showDivs(n) {
     if (n > x.length) {slideIndex = 1}
     if (n < 1) {slideIndex = x.length}
     for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";  
+        x[i].style.display = "none";  
     }
     for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" w3-white", "");
+        dots[i].className = dots[i].className.replace(" w3-white", "");
     }
     x[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " w3-white";
